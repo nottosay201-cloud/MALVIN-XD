1b4f766f08ad5f83e82eef05383e5f5d5cb7308b
nottosay201-cloud <nottosay201@gmail.com>
2025-08-21 22:28:36 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~F6lRjapI#1Vew4gxF6HxdkU0tscWi-OsU3--cKiwieDib1xK7I40",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "918078182087", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
